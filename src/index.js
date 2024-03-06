39770298c5f809e9bfc1a6ef98ca10552684ce0e
rxjans <120579803+rxjans@users.noreply.github.com>
2024-03-07 01:06:47 +0530

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import inputText from './components/input';
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +12,61 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
